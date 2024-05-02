732e0e1e8a637a9a30f9456aad47023700ec1231
Ali <121503489+AlirezaAnzali@users.noreply.github.com>
2024-05-02 13:42:45 -0400

    add landing page and meal page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,25 @@
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>Meals Page</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
